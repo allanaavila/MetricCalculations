cd7b4cc89ded1e5215dbd007da88ef8b906c90f6
Allana Avila <allanavila@gmail.com>
2025-10-05 10:37:03 -0300

    calculation coupling between objects metric

---
+++ src/main/java/org/example/metrics/coupling/CouplingBetweenObjectsMetric.java
@@ -0,0 +1,18 @@
+package org.example.metrics.coupling;
+
+import org.example.AbstractMetricCalculator;
+import org.example.data.ClassAnalysisData;
+
+public class CouplingBetweenObjectsMetric extends AbstractMetricCalculator {
+
+    public CouplingBetweenObjectsMetric(ClassAnalysisData data) {
+        super(data);
+    }
+
+    @Override
+    public double calculateMetric() {
+        // O cálculo é o tamanho do Set que simula as classes usadas
+        return analysisData.coupledClasses.size();
+    }
+
+}
